bd6ce925cff1c295671c9af3b287576a5317e3e7
Nikolai <darks2048@gmail.com>
2020-02-08 15:42:05 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+'use strict';
+var a=12;
+console.log(a);
+
+
+//https://cloud.mail.ru/public/4yRX/3FFcN4FyA#mmm67ebp27c29gmt
+
+//console.log(1438231231231232158);
+    //Вывод в консоль
+
+//let person = {
+  //  name:'Egor',
+    //age: 29,
+    //marry: false 
+    //Коробки с данными
+//}
+//console.log(person.name);
+//console.log(person.age);
+    //Вывод данных из коробки в консоль
+
+//let arr =["apple.png","orange.jpg","banana.bmp"];
+//console.log(arr[0]);
+    //Вывод массивов в консоль
+
+//let answ = confirm("Вы здесь?"); 
+//console.log(answ);
+    //Окно соглашения ДА\НЕТ 
+
+//let prom = prompt("Есть ли вам 20?", "Да,есть");
+//console.log(prom);
+    //Ввод данных на всплывающий вопрос, а нижняя строка проверяет тип данных и выводит в консоль
+//console.log(typeof(prom));
+
+// null;
+// console.log(typeof(null));
+// console.log('arr'+' - object');
+// console.log(4+' - object');
+
+// let kret = 10,
+//     hger = 10;
+
+// console.log(--kret);
+// console.log(++hger);  
+
+// console.log("2"== 2);
+
+let isChecked = true;
+let isClose = true;
+let sexu = "а как какать?";
+
+    console.log(isChecked && !isClose);
+    document.write(sexu);
